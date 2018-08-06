c0d8ac008131625ca1db8d3bd5e79965bbd0c1f3
takkyuuplayer <takkyuuplayer@gmail.com>
2018-08-06 17:40:47 -0400

    To work with typescript

---
+++ web/src/layouts/index.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import Helmet from "react-helmet";
+
+import Header from "../components/header";
+import "./index.css";
+
+interface ILayoutProps {
+  children: any;
+  data: {
+    site: {
+      siteMetadata: {
+        title: string,
+      },
+    },
+  };
+}
+
+const Layout = ({ children, data }: ILayoutProps) => (
+  <div>
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" },
+      ]}
+    />
+    <Header siteTitle={data.site.siteMetadata.title} />
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: 960,
+        padding: "0px 1.0875rem 1.45rem",
+        paddingTop: 0,
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+);
+
+export default Layout;
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
