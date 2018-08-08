ec728421cf32c3b31bb5919be673c3bd9ac7cb04
Takafumi <t.sekiguchi@sproutatwork.com>
2018-08-07 23:22:07 -0400

    To remove existing style

--- web/src/layouts/index.tsx
+++ web/src/layouts/index.tsx
@@ -27,14 +27,7 @@ const Layout = ({ children, data }: ILayoutProps) => (
           { name: "keywords", content: "sample, something" },
         ]}
       />
-      <div
-        style={{
-          margin: "0 auto",
-          maxWidth: 960,
-          padding: "0px 1.0875rem 1.45rem",
-          paddingTop: 0,
-        }}
-      >
+      <div>
         {children()}
       </div>
     </Container>
