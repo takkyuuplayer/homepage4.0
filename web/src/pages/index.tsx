03327f0650f0be00544be5a73ba4bb0a5dd44387
takkyuuplayer <takkyuuplayer@gmail.com>
2018-08-19 05:42:19 -0400

    Add twitter timeline

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from "gatsby-link";
 import * as React from "react";
+import { Timeline } from "react-twitter-widgets";
 
 import { Col, Row } from "reactstrap";
 import History from "../components/History";
@@ -9,7 +10,18 @@ const IndexPage = () => (
     <Col md="8">
       <History />
     </Col>
-    <Col md="4"></Col>
+    <Col md="4">
+      <Timeline
+        dataSource={{
+          screenName: "takkyuuplayer",
+          sourceType: "profile",
+        }}
+        options={{
+          height: "500",
+          username: "takkyuuplayer",
+        }}
+      />
+    </Col>
   </Row>
 );
 
