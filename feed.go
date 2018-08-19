ba2d5b6b85e282897fb4e07f443337fd2bcdabd6
takkyuuplayer <takkyuuplayer@gmail.com>
2018-08-19 07:14:15 -0400

    Fix CORS

--- feed.go
+++ feed.go
@@ -51,30 +51,6 @@ func fetch(url string, ch chan<- []*gofeed.Item) {
 	ch <- feed.Items
 }
 
-func router(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	switch req.HTTPMethod {
-	case "GET":
-		return feed(req)
-	case "OPTIONS":
-		return cors(req)
-	default:
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusMethodNotAllowed,
-			Body:       http.StatusText(http.StatusMethodNotAllowed),
-		}, nil
-	}
-}
-
-func cors(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusNoContent,
-		Headers: map[string]string{
-			"Access-Control-Allow-Methods": "GET",
-			"Access-Control-Allow-Origin":  "*",
-		},
-	}, nil
-}
-
 func feed(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	body, err := json.Marshal(map[string]feedItems{"data": items})
 	if err != nil {
@@ -86,13 +62,16 @@ func feed(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, er
 		}, err
 	}
 	return events.APIGatewayProxyResponse{
-		StatusCode:      http.StatusOK,
-		Headers:         nil,
+		StatusCode: http.StatusOK,
+		Headers: map[string]string{
+			"Access-Control-Allow-Methods": "GET",
+			"Access-Control-Allow-Origin":  "*",
+		},
 		Body:            string(body),
 		IsBase64Encoded: false,
 	}, nil
 }
 
 func main() {
-	lambda.Start(router)
+	lambda.Start(feed)
 }
