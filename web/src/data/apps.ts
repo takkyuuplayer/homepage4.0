98ee014a79400f68457acc6aaf6ec1cd442d53f1
takkyuuplayer <takkyuuplayer@gmail.com>
2018-08-19 02:39:05 -0400

    Fix data

--- web/src/data/apps.ts
+++ web/src/data/apps.ts
@@ -1,20 +1,21 @@
 import * as _ from "lodash";
 
-const tsv = `title	status	type	url	publishedOn	version	lastUpdatedOn	env
-PcEnd	inactive	standalone	pcend.zip	2004/10/06	1.05	2004/12/20	WindowsXP HomeEdition
-Cipher	inactive	standalone	cipher.zip	2004/12/20	1.01	2004/12/30	WindowsXP HomeEdition
-Gufa	inactive	standalone	gufa.zip	2008/05/17	2.03	2009/03/02	WindowsXP, Windows Vista
-AutoIE	inactive	standalone	autoie.zip				WindowsXP, Windows Vista
-mixc	inactive	standalone	mixc.zip	2009/03/31	1.10	2009/10/25	WindowsXP, Windows Vista
-mixi_checker	inactive	standalone	mixi_checker.zip	2009/03/31	1.10	2009/10/25	WindowsXP, Windows Vista
-Sta6DView	inactive	standalone					WindowsXP, Windows Vista
-go-anki	active	web	https://github.com/takkyuuplayer/go-anki
-github-inviter	active	web	https://github.com/takkyuuplayer/github-inviter#github-inviter
-google-drive-transfer	active	web	https://github.com/takkyuuplayer/google-drive-transfer
-anki	active	standalone	https://github.com/takkyuuplayer/anki
-hackyou	active	web	https://github.com/takkyuuplayer/hackyou
-hackme	active	web	https://github.com/takkyuuplayer/hackme				`;
+const baseUri = "https://github.com/takkyuuplayer/homepage/raw/master/public/software";
 
+const tsv = `title	status	type	url	publishedOn	version	lastUpdatedOn	env
+PcEnd	inactive	standalone	pcend.zip	2004/10/06	1.05	2004/12/20	WindowsXP
+Cipher	inactive	standalone	cipher.zip	2004/12/20	1.01	2004/12/30	WindowsXP
+Gufa	inactive	standalone	gufa.zip	2008/05/17	2.03	2009/03/02	WindowsXP, Vista
+AutoIE	inactive	standalone	autoie.zip				WindowsXP, Vista
+mixc	inactive	standalone	mixc.zip	2009/03/31	1.10	2009/10/25	WindowsXP, Vista
+mixi_checker	inactive	standalone	mixi_checker.zip	2009/03/31	1.10	2009/10/25	WindowsXP, Vista
+Sta6DView	inactive	standalone					WindowsXP, Vista
+go-anki	active	web	https://github.com/takkyuuplayer/go-anki#go-anki
+github-inviter	active	web	https://github.com/takkyuuplayer/github-inviter#github-inviter
+google-drive-transfer	active	web	https://github.com/takkyuuplayer/google-drive-transfer#google-drive-transfer
+anki	active	standalone	https://github.com/takkyuuplayer/anki/archive/master.zip
+hackme	active	web	https://github.com/takkyuuplayer/hackme#hackme-
+hackyou	active	web	https://github.com/takkyuuplayer/hackyou#hackyou				`;
 const rows = tsv.split("\n").map((line: string) => line.trim().split("\t"));
 const header = rows.shift();
 
@@ -26,6 +27,7 @@ const rowToAppRow = (row: typeof rows[0]) => {
         {
             lastUpdatedOn: zipped.lastUpdatedOn === "" ? undefined : new Date(zipped.lastUpdatedOn),
             publishedOn: zipped.publishedOn === "" ? undefined : new Date(zipped.publishedOn),
+            url: zipped.url.startsWith("http") ? zipped.url : `${baseUri}/${zipped.url}`,
         },
     );
 };
