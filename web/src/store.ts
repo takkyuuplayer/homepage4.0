c9aea4161fa2ba6304d0f5272131c958d0822d22
takkyuuplayer <takkyuuplayer@gmail.com>
2018-08-19 19:46:40 -0400

    Clean store

--- web/src/store.ts
+++ web/src/store.ts
@@ -3,23 +3,29 @@ import { intlReducer } from "react-intl-redux";
 import * as enLocaleData from "react-intl/locale-data/en";
 import * as jaLocaleData from "react-intl/locale-data/ja";
 import { combineReducers, createStore } from "redux";
-import blog, { setBlogFeed } from "./ducks/blog";
-import { initialState as intl, setLocaleByBrowserLanguages } from "./ducks/i18n";
+import { ActionType, StateType } from "typesafe-actions";
+import blog, * as blogActions from "./ducks/blog";
+import intl, * as i18nActions from "./ducks/i18n";
 
 addLocaleData([...jaLocaleData, ...enLocaleData]);
 
-const reducer = combineReducers({
+const rootReducer = { blog, intl };
+const rootActions = { blogActions, i18nActions };
+type RootState = StateType<typeof rootReducer>;
+type RootActions = ActionType<typeof rootActions>;
+
+const reducer = combineReducers<RootState, RootActions>({
     blog,
-    intl: intlReducer,
+    intl,
 });
 
-const store = createStore(reducer, { intl });
-store.dispatch(setLocaleByBrowserLanguages(navigator.languages));
+const store = createStore(reducer);
+store.dispatch(i18nActions.setLocaleByBrowserLanguages(navigator.languages));
 
 (async () => {
     const res = await fetch("https://wudix076af.execute-api.ap-northeast-1.amazonaws.com/Prod/feed");
     const body = await res.json();
-    store.dispatch(setBlogFeed(body.data));
+    store.dispatch(blogActions.setBlogFeed(body.data));
 })();
 
 export default store;
