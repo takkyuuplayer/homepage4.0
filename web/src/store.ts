0cbe75d398b90f26f6c6a1561dfa2de7c9e7dd69
takkyuuplayer <takkyuuplayer@gmail.com>
2018-08-19 22:13:59 -0400

    To build

--- web/src/store.ts
+++ web/src/store.ts
@@ -6,16 +6,21 @@ import reducer, { actions } from "./ducks";
 
 addLocaleData([...jaLocaleData, ...enLocaleData]);
 
-const cachedState = localStorage.getItem("redux");
-const initialState = cachedState ? JSON.parse(cachedState) : undefined;
+let initialState;
+if (typeof localStorage !== `undefined`) {
+    const cachedState = localStorage.getItem("redux");
+    initialState = cachedState ? JSON.parse(cachedState) : undefined;
+}
 const store = createStore(reducer, initialState);
 
-if (!initialState) {
+if (typeof localStorage !== `undefined`) {
     store.subscribe(() => localStorage.setItem("redux", JSON.stringify(store.getState())));
 }
 
-fetch("https://wudix076af.execute-api.ap-northeast-1.amazonaws.com/Prod/feed")
-    .then((res) => res.json())
-    .then((body) => store.dispatch(actions.blogActions.setBlogFeed(body.data)));
+if (typeof fetch !== `undefined`) {
+    fetch("https://wudix076af.execute-api.ap-northeast-1.amazonaws.com/Prod/feed")
+        .then((res) => res.json())
+        .then((body) => store.dispatch(actions.blogActions.setBlogFeed(body.data)));
+}
 
 export default store;
