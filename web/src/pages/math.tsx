db922c4c16ed1fb27124200a2a4bfc006c75012f
Takafumi <t.sekiguchi@sproutatwork.com>
2018-09-11 17:10:07 -0400

    Fix math links

--- web/src/pages/math.tsx
+++ web/src/pages/math.tsx
@@ -1,18 +1,17 @@
+import { withPrefix } from "gatsby-link";
 import * as _ from "lodash";
 import * as React from "react";
 import { FormattedMessage } from "react-intl";
 import { Table } from "reactstrap";
 
-const BaseUri = `https://github.com/takkyuuplayer/homepage/raw/master/public/img/math`;
-
 const questions = _.range(0, 18).map((num: number) => {
     const key = num < 9 ? "0" + (num + 1) : (num + 1);
     return <tr key={key}>
         <td>
-            <img src={`${BaseUri}/toi${key}.png`} />
+            <img src={withPrefix(`math/toi${key}.png`)} />
         </td>
         <td className="align-middle">
-            <a href={`${BaseUri}/ans${key}.pdf`}>
+            <a href={withPrefix(`math/ans${key}.pdf`)}>
                 <FormattedMessage id="math.answer" />
             </a>
         </td>
