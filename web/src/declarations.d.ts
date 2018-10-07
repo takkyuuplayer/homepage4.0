7b32b3495cb527cf6ee4d053ad709c99a1ed0657
takkyuuplayer <takkyuuplayer@gmail.com>
2018-10-07 02:07:54 -0400

    Fix JSON module import

--- web/src/declarations.d.ts
+++ web/src/declarations.d.ts
@@ -1,11 +1,3 @@
 declare const graphql: (query: TemplateStringsArray) => void;
 declare module '*.gif';
-declare module '*/locales.json' {
-    interface Locale {
-        [lang: string]: {
-            [key: string]: string;
-        }
-    }
-    const value: Locale;
-    export default value;
-}
+declare module '*/locales.json';
