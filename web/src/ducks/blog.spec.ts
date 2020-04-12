6f30e86641c016e0c3a4a0d9476bce7116966a39
takkyuuplayer <takkyuuplayer@gmail.com>
2020-04-12 01:37:40 -0400

    Stop using typesafe-actions

--- web/src/ducks/blog.spec.ts
+++ web/src/ducks/blog.spec.ts
@@ -1,23 +1,22 @@
-import reducer, { setBlogFeed } from "./blog";
+import reducer, { setBlogFeed } from './blog'
 
-describe("ducks/blog", () => {
-    const feeds = [
-        { title: "1", link: "2", published: "3" },
-    ];
-    describe("actions", () => {
-        it("has setBlogFeed", () => {
-            expect(setBlogFeed(feeds))
-                .toEqual({ payload: feeds, type: "SET_BLOG_FEED" });
-        });
-    });
+describe('ducks/blog', () => {
+  const feeds = [{ title: '1', link: '2', published: '3' }]
+  describe('actions', () => {
+    it('has setBlogFeed', () => {
+      expect(setBlogFeed(feeds)).toEqual({
+        payload: feeds,
+        type: 'SET_BLOG_FEED',
+      })
+    })
+  })
 
-    describe("reducer", () => {
-        it("returns initial state", () => {
-            expect(reducer(undefined, {} as any))
-                .toStrictEqual([]);
-        });
-        it("handles SET_BLOG_FEED action", () => {
-            expect(reducer([], setBlogFeed(feeds))).toStrictEqual(feeds);
-        });
-    });
-});
+  describe('reducer', () => {
+    it('returns initial state', () => {
+      expect(reducer(undefined, {} as any)).toStrictEqual([])
+    })
+    it('handles SET_BLOG_FEED action', () => {
+      expect(reducer([], setBlogFeed(feeds))).toStrictEqual(feeds)
+    })
+  })
+})
