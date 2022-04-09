749ef99837073bcd3194f316de20ac599a6a48c8
takkyuuplayer <takkyuuplayer@gmail.com>
2022-04-09 22:13:32 +0900

    fix found issues

--- web/src/pages/app.tsx
+++ web/src/pages/app.tsx
@@ -1,14 +1,17 @@
-import { t } from 'i18next'
 import * as React from 'react'
+import { useTranslation } from 'react-i18next'
 import ActiveApps from '../components/ActiveApps'
 import InactiveApps from '../components/InactiveApps'
 import Layout from '../components/layout'
 
-export default () => (
-  <Layout>
-    <h4>{t('app.active')}</h4>
-    <ActiveApps />
-    <h4 style={{ marginTop: '2em' }}>{t('app.inactive')}</h4>
-    <InactiveApps />
-  </Layout>
-)
+export default () => {
+  const { t } = useTranslation()
+  return (
+    <Layout>
+      <h4>{t('app.active')}</h4>
+      <ActiveApps />
+      <h4 style={{ marginTop: '2em' }}>{t('app.inactive')}</h4>
+      <InactiveApps />
+    </Layout>
+  )
+}
