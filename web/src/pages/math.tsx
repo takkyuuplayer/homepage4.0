2b77b8fc2c778afb030ae793caeea8cf429de1e9
takkyuuplayer <takkyuuplayer@gmail.com>
2022-04-09 18:12:00 +0900

    fix found issue

--- web/src/pages/math.tsx
+++ web/src/pages/math.tsx
@@ -1,27 +1,24 @@
 import { withPrefix } from 'gatsby-link'
-import * as _ from 'lodash'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Table } from 'reactstrap'
 import Layout from '../components/layout'
 
-const questions = _.range(0, 18).map((num: number) => {
-  const { t } = useTranslation()
-  const key = num < 9 ? '0' + (num + 1) : num + 1
-  return (
-    <tr key={key}>
-      <td>
-        <img src={withPrefix(`math/toi${key}.png`)} />
-      </td>
-      <td className="align-middle">
-        <a href={withPrefix(`math/ans${key}.pdf`)}>{t('math.answer')}</a>
-      </td>
-    </tr>
-  )
-})
-
 export default () => {
   const { t } = useTranslation()
+  const questions = [...Array(18).keys()].map((num: number) => {
+    const key = num < 9 ? '0' + (num + 1) : num + 1
+    return (
+      <tr key={key}>
+        <td>
+          <img src={withPrefix(`math/toi${key}.png`)} />
+        </td>
+        <td className="align-middle">
+          <a href={withPrefix(`math/ans${key}.pdf`)}>{t('math.answer')}</a>
+        </td>
+      </tr>
+    )
+  })
   return (
     <Layout>
       <h4>{t('math.kingdom')}</h4>
