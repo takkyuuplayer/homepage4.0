f509c3d37078e5d33e97ddce5248b02a0b93ee71
takkyuuplayer <takkyuuplayer@gmail.com>
2023-11-03 12:26:19 +0900

    use the latest apigateway

--- web/src/components/Blog.tsx
+++ web/src/components/Blog.tsx
@@ -19,7 +19,7 @@ export default () => {
 
   React.useEffect(() => {
     fetch(
-      'https://wudix076af.execute-api.ap-northeast-1.amazonaws.com/Prod/feed'
+      'https://o52wfywme7.execute-api.ap-northeast-1.amazonaws.com/prod/feed'
     )
       .then((res) => res.json())
       .then((body) => setFeeds(body.data))
